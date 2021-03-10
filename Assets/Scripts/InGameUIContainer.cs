9f50891e1f13cd975895faae67800ef2a8a372c8
name <jackjsh@naver.com>
2021-03-10 16:42:37 +0900

    일시정지, 메뉴복귀 구현
    InGameScene에 UI 추가
    아이콘 에셋 추가(Clean Vector Icons)
    ->사용한 것은 밖으로 이동

---
+++ Assets/Scripts/InGameUIContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUIContainer : MonoBehaviour
+{
+    public static InGameUIContainer instance;
+
+    [Header("Canvas")]
+    public Canvas Canvas_FormatedDolls;
+    public Canvas Canvas_DollInfo;
+    public Canvas Canvas_GameInfo;
+
+    //[Header("FormatedDolls-----Panel-----")]
+    //[Header("                          -----Button-----")]
+
+    //[Header("DollInfo-----Panel-----")]
+    //[Header("                   -----Button-----")]
+
+    [Header("GameInfo-----Panel-----")]
+    public GameObject Panel_Pause;
+    [Header("                   -----Button-----")]
+    public Button Button_Pause;
+    public Button Button_Resume;
+    public Button Button_Quit;
+
+    void Awake() {
+        instance = this;
+        Time.timeScale = 1;
+    }
+
+
+    public void Open_Panel_Pause() {
+        Time.timeScale = 0;
+        Panel_Pause.SetActive(true);
+    }
+    public void Close_Panel_Pause() {
+        Time.timeScale = 1;
+        Panel_Pause.SetActive(false);
+    }
+}
