a21244cb11617c5c15b8c847782908c17a2e7c87
name <jackjsh@naver.com>
2021-04-04 22:53:57 +0900

    DMR 타깃 검색 버그 수정

--- Assets/Scripts/SRController.cs
+++ Assets/Scripts/SRController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BuffContainer))]
-public class DMRController : DollController {
-
+public class SRController : DollController
+{
     GameObject temp_target;
 
     public override void SearchTarget() {
@@ -15,22 +15,16 @@ public class DMRController : DollController {
         attackable = false;
         //print("Check Target" + gameObject.name);
         for (int i = 0; i < InGameManager.instance.Spawned_Enemies.Count; i++) {
-            if (GetDistance(InGameManager.instance.Spawned_Enemies[i]) <= fs.range
-                && InGameManager.instance.Spawned_Enemies[i].activeSelf) {
+            if (InGameManager.instance.Spawned_Enemies[i].activeSelf) {
                 attackable = true;
 
-                if(temp_target == null)
+                if (temp_target == null)
                     temp_target = InGameManager.instance.Spawned_Enemies[0];
 
                 //print(InGameManager.instance.Spawned_Enemies[i].GetComponent<FinalState>().hp);
-                if (InGameManager.instance.Spawned_Enemies[i].GetComponent<FinalState>().hp
-                    < temp_target.GetComponent<FinalState>().hp) {
+                if (InGameManager.instance.Spawned_Enemies[i].GetComponent<OriginalState>().maxHP
+                    > temp_target.GetComponent<OriginalState>().maxHP) {
                     temp_target = InGameManager.instance.Spawned_Enemies[i];
-                }
-                else {
-                    if (temp_target.GetComponent<FinalState>().hp <= 0) {
-                        temp_target = InGameManager.instance.Spawned_Enemies[i];
-                    }
                 }
             }
             else {
