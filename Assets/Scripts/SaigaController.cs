b8c883b399b24214dbabd643c48554bbaf10ba77
name <jackjsh@naver.com>
2021-04-13 00:58:12 +0900

    Saiga12 스킬 추가
    약점조준 : 다음 3번의 공격 강화

---
+++ Assets/Scripts/SaigaController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaigaController : SGController
+{
+    public int skillcount = 0;
+    public bool skilling = false;
+
+    public override void attack() {
+        if(!skilling)
+            base.attack();
+
+        else {
+            if(skillcount == 0) {
+                _attack(fs.damage * GetComponent<BigAntelopesHorn>().increase_dmg1[GetComponent<BigAntelopesHorn>().skilllevel]);
+                skillcount++;
+            }
+            else if(skillcount == 1) {
+                _attack(fs.damage * GetComponent<BigAntelopesHorn>().increase_dmg1[GetComponent<BigAntelopesHorn>().skilllevel]);
+                skillcount++;
+            }
+            else if(skillcount == 2) {
+                _attack(fs.damage * GetComponent<BigAntelopesHorn>().increase_dmg1[GetComponent<BigAntelopesHorn>().skilllevel]);
+                skilling = false;
+            }
+            else {
+                Debug.LogError("Saiga12 Skill Error");
+            }
+        }
+    }
+
+    void _attack(float dmg) {
+        SetFaceDir(Target.transform.position.x);
+        for (int i = 0; i < Blocked_Enemies.Count; i++) {
+            Blocked_Enemies[i].GetComponent<CharacterBase>().GetAttacked((int)dmg, fs.accuracy, fs.critrate, fs.armorpen);
+        }
+        fs.ammo--;
+    }
+
+}
---
+++ Assets/Scripts/Skill/BigAntelopesHorn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigAntelopesHorn : SkillBase
+{
+    public float[] increase_dmg1 = { 0.8f, 0.9f, 1, 1.1f, 1.2f, 1.3f, 1.4f, 1.4f, 1.5f, 1.5f };
+    public float[] increase_dmg2 = { 1.2f, 1.3f, 1.5f, 1.6f, 1.8f, 1.9f, 2.1f, 2.2f, 2.4f, 2.5f };
+    public float[] increase_dmg3 = { 1.6f, 1.7f, 2, 2.1f, 2.4f, 2.5f, 2f, 8.3f, 3.3f, 3.5f };
+
+    public override void SkillActive() {
+        if (GetComponent<SaigaController>().skilling || GetComponent<SaigaController>().List_temp_target.Count == 0)
+            return;
+
+        print("use skill : " + GetSkillName() + "    LV." + skilllevel);
+        //배치된 모든 인형들에게 버프 부여
+        skill_cool_timer = GetCoolDown();
+        skill_duration_timer = GetDuration();
+
+        GetComponent<SaigaController>().skilling = true;
+        GetComponent<SaigaController>().skillcount = 0;
+    }
+
+    public override void SkillDescribe() {
+        base.SkillDescribe();
+
+        skill_describe = skill_describe.Replace("_dmg1", increase_dmg1[skilllevel].ToString());
+        skill_describe = skill_describe.Replace("_dmg2", increase_dmg2[skilllevel].ToString());
+        skill_describe = skill_describe.Replace("_dmg3", increase_dmg3[skilllevel].ToString());
+    }
+}
