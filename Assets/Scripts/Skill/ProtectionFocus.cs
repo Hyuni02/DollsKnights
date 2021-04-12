fab605cc233adb3d0d9a3e5610f9188f56093c6f
name <jackjsh@naver.com>
2021-04-12 16:16:38 +0900

    샷건 추가/M590,Saiga12
    M590 스킬 구현 : 집중방어

--- Assets/Scripts/Skill/ProtectionFocus.cs
+++ Assets/Scripts/Skill/ProtectionFocus.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageFocus : SkillBase
+public class ProtectionFocus : SkillBase
 {
-    GameObject Target;
-    public int[] increase_dmg = { 40, 43, 47, 50, 53, 57, 60, 63, 67, 70 };
+    public int[] increase_armor = { 40, 43, 47, 50, 53, 57, 60, 63, 67, 70 };
 
     public override void SkillActive() {
         print("use skill : " + GetSkillName() + "    LV." + skilllevel);
         //배치된 모든 인형들에게 버프 부여
         skill_cool_timer = GetCoolDown();
         skill_duration_timer = GetDuration();
+
         //버프 소환
         GameObject buff = Instantiate(InGameManager.instance.buff);
-        buff.GetComponent<Buff>().Initialized(GetSkillName(), GetDuration(), GetComponent<SkillBase>().skill_icon, gameObject, gameObject, increase_dmg[skilllevel], 0, 0, 0, 0, 0, 0, false, 0);
+        buff.GetComponent<Buff>().Initialized(GetSkillName(), GetDuration(), GetComponent<SkillBase>().skill_icon, gameObject, gameObject, 0, 0, increase_armor[skilllevel], 0, 0, 0, 0, false, 0);
     }
+
     public override void SkillDescribe() {
         base.SkillDescribe();
-
-        skill_describe = skill_describe_form.Replace("_dmg", increase_dmg[skilllevel].ToString());
+        skill_describe = skill_describe.Replace("_armor", increase_armor[skilllevel].ToString());
     }
 }
