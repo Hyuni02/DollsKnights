873b6fe377f9399e90ead61d0776ea07881f98d6
name <jackjsh@naver.com>
2021-04-12 15:32:40 +0900

    SR 타겟 검색 방식 수정
    체력이 적은 적 우선 공격

--- Assets/Scripts/SRController.cs
+++ Assets/Scripts/SRController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class SRController : DMRController
 {
     public override void GetInRangeTarget() {
+        List_temp_target.Clear();
         for (int i = 0; i < InGameManager.instance.Spawned_Enemies.Count; i++) {
             if (InGameManager.instance.Spawned_Enemies[i].activeSelf) {
                 List_temp_target.Add(InGameManager.instance.Spawned_Enemies[i]);
