e4e74c89e4b7d63c5161bcc8db26fc85d24eaae5
name <jackjsh@naver.com>
2021-04-17 12:45:23 +0900

    Scarecrow,Hunter 데이터 추가

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : CharacterBase
+{
+    public override void UpdateState() {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Getstun() {
+        throw new System.NotImplementedException();
+    }
+}
